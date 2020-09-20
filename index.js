c8d643b6f64e881c2d36e751a8cfe24ff84df1c3
Brendan Carney <brendan@convertkit.com>
2020-09-20 08:56:17 -0500

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec').exec
+
+
+async function run() {
+  try {
+
+    let eslintOutput = ''
+    let eslintError = ''
+
+    await exec(
+      'node',
+      [
+        path.join(process.cwd(), 'node_modules/eslint/bin/eslint'),
+        `--fix-dry-run`,
+        '--format',
+        'json',
+      ],
+      {
+        silent: true,
+        listeners: {
+          stdout: (data) => {
+            eslintOutput += data.toString()
+          },
+          stderr: (data) => {
+            eslintError += data.toString()
+          }
+        }
+      }
+    )
+
+    console.log(eslintOutput);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
